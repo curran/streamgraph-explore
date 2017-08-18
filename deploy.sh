81117913cdb0fb50d617bedbf726b1324f1454c5
curran <curran.kelleher@gmail.com>
2017-08-18 12:42:19 +0530

    Update deploy to clone

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-#git clone git@github.com:curran/streamgraph-explorer.git
-#cd streamgraph-explorer/
-#npm install
-#npm run build
-#cp index.html ..
-#cp -r data/ ..
-#cp -r build/ ..
-#cd ..
-#rm -rf streamgraph-explorer/
+git clone git@github.com:curran/streamgraph-explorer.git
+cd streamgraph-explorer/
+npm install
+npm run build
+cp index.html ..
+cp -r data/ ..
+cp -r build/ ..
+cd ..
+rm -rf streamgraph-explorer/
 
-cp ../streamgraph-explorer/index.html .
-cp -r ../streamgraph-explorer/data/ .
-cp -r ../streamgraph-explorer/build/ .
+#cp ../streamgraph-explorer/index.html .
+#cp -r ../streamgraph-explorer/data/ .
+#cp -r ../streamgraph-explorer/build/ .
 
 git add .
 git commit -m "Deploy new build"
